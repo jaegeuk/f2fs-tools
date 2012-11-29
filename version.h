da59f6146c37e727bb83ae4922ca56d42958e61c
Jaegeuk Kim <jaegeuk.kim@samsung.com>
2012-11-29 12:44:04 +0900

    Sync the version management
    For debian packaing, I should have matched the previous verions, v1.1.0.
    So, this is the start of f2fs-tools version.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- version.h
+++ version.h
@@ -6,5 +6,5 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#define F2FS_TOOLS_VERSION "1.0.0"
-#define F2FS_TOOLS_DATE "26-Nov-2012"
+#define F2FS_TOOLS_VERSION "1.1.0"
+#define F2FS_TOOLS_DATE "29-Nov-2012"
