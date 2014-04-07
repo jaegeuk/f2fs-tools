e35b8afb34822ccc0835b453c7a3417bc0825569
Jaegeuk Kim <jaegeuk.kim@samsung.com>
2014-04-07 12:10:59 +0900

    f2fs-tools: announce dual licenses with GPL and LGPL v2
    This patch announces LGPL v2 for the following files.
    - include/f2fs_fs.h
    - lib/libf2fs.c
    - lib/libf2fs_io.c
    - mkfs/f2fs_format.c
    - mkfs/f2fs_format_main.c
    - mkfs/f2fs_format_utils.c
    - mkfs/f2fs_format_utils.h
    
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>

--- include/f2fs_fs.h
+++ include/f2fs_fs.h
@@ -4,9 +4,7 @@
  * Copyright (c) 2012 Samsung Electronics Co., Ltd.
  *             http://www.samsung.com/
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * Dual licensed under the GPL or LGPL version 2 licenses.
  */
 #ifndef __F2FS_FS_H__
 #define __F2FS_FS_H__
--- mkfs/f2fs_format_utils.h
+++ mkfs/f2fs_format_utils.h
@@ -4,9 +4,7 @@
  * Copyright (c) 2014 Samsung Electronics Co., Ltd.
  *             http://www.samsung.com/
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * Dual licensed under the GPL or LGPL version 2 licenses.
  */
 #define _LARGEFILE64_SOURCE
 
