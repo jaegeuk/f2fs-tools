9a5808cfb7e49bf640a10767d13e16579e8cda30
Changman Lee <cm224.lee@samsung.com>
2014-11-04 18:10:54 +0900

    mkfs.f2fs: reclaim free space in case of regular file
    If we use regular file instead block device, let's reclaim its free
    space.
    
    Signed-off-by: Changman Lee <cm224.lee@samsung.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- mkfs/f2fs_format_utils.c
+++ mkfs/f2fs_format_utils.c
@@ -6,17 +6,25 @@
  *
  * Dual licensed under the GPL or LGPL version 2 licenses.
  */
+#define _LARGEFILE_SOURCE
 #define _LARGEFILE64_SOURCE
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 
 #include "f2fs_fs.h"
 
 #ifdef HAVE_LINUX_FS_H
 #include <linux/fs.h>
+#endif
+#ifdef HAVE_LINUX_FALLOC_H
+#include <linux/falloc.h>
 #endif
 
 int f2fs_trim_device()
@@ -37,9 +45,15 @@ int f2fs_trim_device()
 
 #if defined(WITH_BLKDISCARD) && defined(BLKDISCARD)
 	MSG(0, "Info: Discarding device\n");
-	if (S_ISREG(stat_buf.st_mode))
+	if (S_ISREG(stat_buf.st_mode)) {
+#ifdef FALLOC_FL_PUNCH_HOLE
+		if (fallocate(config.fd, FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE,
+				range[0], range[1]) < 0) {
+			MSG(0, "Info: fallocate(PUNCH_HOLE|KEEP_SIZE) is failed\n");
+		}
+#endif
 		return 0;
-	else if (S_ISBLK(stat_buf.st_mode)) {
+	} else if (S_ISBLK(stat_buf.st_mode)) {
 		if (ioctl(config.fd, BLKDISCARD, &range) < 0) {
 			MSG(0, "Info: This device doesn't support TRIM\n");
 		} else {
